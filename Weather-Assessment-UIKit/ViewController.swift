1c6d96ad1e4565dfe68f1f75a936337dea2163cd
jean <jean.blignaut@me.com>
2021-06-11 11:25:27 +0200

    Initial Commit

---
+++ Weather-Assessment-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather-Assessment-UIKit
+//
+//  Created by Jean Blignaut on 2021/06/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
